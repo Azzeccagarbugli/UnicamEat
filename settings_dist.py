9370235415108b2c1b72c3991dcaafbf18361d3c
Azzeccagarbugli <f.coppola1998@gmail.com>
2018-02-06 00:35:34 +0100

    [FIX] Piccole correzioni nel file

--- settings_dist.py
+++ settings_dist.py
@@ -19,9 +19,9 @@ closed_msg = "La mensa del D'Avack nei giorni <b>Venerdì</b>, <b>Sabato</b> e <
 # Opening time
 opening_msg = "<b>D'Avack</b>\n"\
             "Aperta tutti i giorni della settimana durante il pranzo, esclusi <b>Venerdì</b>, <b>Sabato</b> e <b>Domenica</b>, dalle ore <b>12:30</b> alle ore <b>14:15</b>. "\
-            "\n<i>Posizione</i>: /posizione_avak\n"\
+            "\n<i>Posizione:</i> /posizione_avak\n"\
             ""\
             "\n<b>Colle Paradiso</b>\n"\
             "Aperta tutti i giorni della settimana dalle ore <b>12:30</b> alle ore <b>14:15</b> e dalle ore <b>19:30</b> alle ore <b>21:15</b>."\
             "\nDurante il week-end la mensa, invece, rimarrà aperta <b>esclusivamente</b> per pranzo dalle ore <b>12:30</b> alle ore <b>13:15</b>."\
-            "\n<i>Posizione</i>: /posizione_colleparadiso"
+            "\n<i>Posizione:</i> /posizione_colleparadiso"
