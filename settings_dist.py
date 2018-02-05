18a3fdb386ed81910d6a9a4b217048d45250986e
Azzeccagarbugli <f.coppola1998@gmail.com>
2018-02-05 19:20:10 +0100

    [UPDATE] Aggiunto messaggio per orari di apertura

--- settings_dist.py
+++ settings_dist.py
@@ -15,3 +15,13 @@ directory_fcopp = '***YOUR DIRECTORY***'
 closed_msg = "La mensa del D'Avack nei giorni <b>Venerdì</b>, <b>Sabato</b> e <b>Domenica</b> rimane chiusa sia "\
             "per pranzo che per cena. Riprova a inserire il comando /seleziona_mensa e controlla la mensa "\
             "di <b>Colle Pardiso</b> per ottenere i menù da te desiderati"
+
+# Opening time
+opening_msg = "<b>D'Avack</b>\n"\
+            "Aperta tutti i giorni della settimana durante il pranzo, esclusi <b>Venerdì</b>, <b>Sabato</b> e <b>Domenica</b>, dalle ore <b>12:30</b> alle ore <b>14:15</b>. "\
+            "\n<i>Posizione</i>: /posizione_avak\n"\
+            ""\
+            "\n<b>Colle Paradiso</b>\n"\
+            "Aperta tutti i giorni della settimana dalle ore <b>12:30</b> alle ore <b>14:15</b> e dalle ore <b>19:30</b> alle ore <b>21:15</b>."\
+            "\nDurante il week-end la mensa, invece, rimarrà aperta <b>esclusivamente</b> per pranzo dalle ore <b>12:30</b> alle ore <b>13:15</b>."\
+            "\n<i>Posizione</i>: /posizione_colleparadiso"
